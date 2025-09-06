af98e37e327b8df6a61d39c95197c812d084bc70
Курникова Милана Станиславовна <1899-23@std.pkgh.edu>
2025-09-06 14:51:20 +0300

    start

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,21 +3,20 @@ fun main(args: Array<String>) {
     var line = readln()
     println()
 
-    if (line.isEmpty()) {
-        println("Строка пуста")
-        return
-    }
-
     var result = ""
     var count = 1
 
-    for (i in 1 until line.length)
-        if (line[i] == line[i - 1]) {
+    for (i in line.indices) {
+        if (i < line.length - 1 && line[i] == line[i+1]) { // Проверяем есть ли след. символ и что он такой же как текущий
             count++
         }
-        result += line.last() + (if (count > 1) count.toString() else "")
+        else {
+            result += line[i] // Иначе добавляем символ в рез-т
+            if (count > 1) {
+                result += count // Если символ дублировался, то добавляем кол-во повторов
+            }
+            count = 1 // Сброс для след. проверки
+        }
+    }
         println("Result -> $result")
-
-
-
 }
